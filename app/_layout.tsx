a6ca0e8dc65e69368a9f712e654ab7ad788d5fde
MultiDevs <multiz.dev@gmail.com>
2024-10-17 17:32:14 +0500

    Add push notification support
    Integrated Expo notifications and device modules to request and handle push notifications. Updated `yarn.lock` and `package.json` to include new dependencies for notification features. Implemented notification handler and permission request in the `_layout.tsx` file.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,9 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
 import auth from "@react-native-firebase/auth";
 
 import { ActivityIndicator, Provider, Toast } from "@ant-design/react-native";
@@ -12,13 +15,48 @@ import useAuth from "@src/modules/auth/hooks/useAuth";
 
 // Import your global CSS file
 import "@/global.css";
+import messaging from "@react-native-firebase/messaging";
 
 SplashScreen.preventAutoHideAsync().then();
+
+// Configure notification handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+async function requestUserPermission(): Promise<boolean> {
+  const authStatus = await messaging().requestPermission();
+  return (
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL
+  );
+}
 
 function RootLayout(): ReactElement {
   const { user, setUser } = useAppStore();
   const { onAuthStateChanged } = useAuth();
   const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    (async (): Promise<void> => {
+      await registerForPushNotificationsAsync();
+      await requestUserPermission();
+    })();
+
+    Notifications.getLastNotificationResponseAsync().then(async (response) => {
+      if (!isMounted || !response?.notification) {
+      }
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   useEffect(() => {
     async function loadFonts() {
@@ -67,6 +105,34 @@ function RootLayout(): ReactElement {
       <StatusBar style="dark" />
     </Provider>
   );
+}
+
+async function registerForPushNotificationsAsync(): Promise<
+  string | undefined
+> {
+  if (Device.osName === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+    });
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+  return (
+    await Notifications.getExpoPushTokenAsync({
+      projectId: "f42aa10b-990d-4624-a5c6-95e2da52de82",
+    })
+  ).data;
 }
 
 export default RootLayout;
